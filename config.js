40e8bbdc8d2c15d84bd4d218d6c3531f5441436b
Estebangda09 <146000223+Estebangda09@users.noreply.github.com>
2026-03-20 22:49:44 -0300

    Update Supabase client initialization method

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
+// config.js
 const SUPABASE_URL = 'https://vokwutkpntqtfevvdkei.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZva3d1dGtwbnRxdGZldnZka2VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3OTE4MDMsImV4cCI6MjA4OTM2NzgwM30.FdNLN4PdKAMe4LSdw9lGqjLeV6UzbfFlwZjgkd8nkro';
 
-// Usamos el objeto global 'supabase' que provee la librería
-const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// Usamos el objeto global 'supabase' de la librería CDN
+const _supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
