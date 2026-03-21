5eed21e70d06f678f72caed7354f9fdfb8d2b74d
Estebangda09 <146000223+Estebangda09@users.noreply.github.com>
2026-03-20 22:47:21 -0300

    Remove blank line in config.js
    Removed a blank line in config.js.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://vokwutkpntqtfevvdkei.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZva3d1dGtwbnRxdGZldnZka2VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3OTE4MDMsImV4cCI6MjA4OTM2NzgwM30.FdNLN4PdKAMe4LSdw9lGqjLeV6UzbfFlwZjgkd8nkro';
 
-
+// Usamos el objeto global 'supabase' que provee la librería
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
